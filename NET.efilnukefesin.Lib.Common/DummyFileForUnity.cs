4e2c6ad1a0b80620205734a5d6f78161b07fdcd5
Michael Kolb <efil.threat@googlemail.com>
2021-07-08 22:47:37 +0200

    made correct unity referencing

---
+++ NET.efilnukefesin.Lib.Common/DummyFileForUnity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NET.efilnukefesin.Lib.Common
+{
+    public class DummyFileForUnity : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}

5766f251c19fdc3a1555d967ed8a939f68f68f3a
Michael Kolb <efil.threat@googlemail.com>
2021-07-15 21:51:10 +0200

    added some interfaces

---
+++ NET.efilnukefesin.Lib.Common.Interfaces/Services/IAuthorizationService.cs
@@ -0,0 +1,19 @@
+﻿using NET.efilnukefesin.Lib.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Lib.Common.Interfaces.Services
+{
+    [ArchitectureLayer("Security")]
+    public interface IAuthorizationService : IService
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
---
+++ NET.efilnukefesin.Lib.Common.Interfaces/Services/IUserNotificationService.cs
@@ -0,0 +1,19 @@
+﻿using NET.efilnukefesin.Lib.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Lib.Common.Interfaces.Services
+{
+    [ArchitectureLayer("Messaging")]
+    public interface IUserNotificationService : IService
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
